f00a14f953676fccf4026ef936d4938beeb73876
Gabrie Tangerina <gabriel.tangerina@hotmail.com>
2023-12-29 00:16:58 -0300

    Aprendendo  styled

---
+++ src/pages/Login/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import { Title } from "./styled";
+
+export default function Login() {
+  return (
+    <>
+      <Title isRed>Página de Login</Title>
+      <p>Qualquer coisa aqui</p>
+    </>
+  );
+}
